2fd12046680c4434a69dc420872b6c639cc35b13
Pat Garner <patgarner@gmail.com>
2019-11-22 22:33:33 +0300

    Add futher information to bring package file format up to date.

--- Package.swift
+++ Package.swift
@@ -2,5 +2,15 @@
 import PackageDescription
 
 let package = Package(
-    name: "CGLFW3"
+    name: "CGLFW3",
+    platforms: [
+        .macOS(.v10_14),
+    ],
+    products: [
+        .library(name: "CGLFW3", targets: ["CGLFW3"]),
+    ],
+    targets: [
+        .target(name: "CGLFW3", dependencies: []),
+        .testTarget(name: "CGLFW3Tests", dependencies: ["CGLFW3"]),
+    ]
 )
